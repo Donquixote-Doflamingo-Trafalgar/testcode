03695b3feb00fa3310a689376a517b62eee4554d
Donquixote-Doflamingo-Trafalgar <omayosennin@gmail.com>
2024-12-25 12:03:53 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,22 @@
+player = "Salah"
+print  (f"{player} is the top scorer.") 
+goals = 15
+print (f"He has scored {goals} goals in the 2024-25 league season.") 
+assists = 11
+print (f"He has assisted {assists} times.")
+ratio = 1.296
+print (f"His GA ratio is {ratio}.") 
+top_ga = True
+if top_ga:
+    print("He has the most goal contributions in the league , with 26.") 
+else:
+    print("Yet he is not the highest goal contributor in the league.") 
+print(type(player))
+print(ratio)
+print(player)
+print(goals)
+print(type(goals))
+print(type(assists))
+print(type(player))
+print(type(top_ga))
+print(type(ratio))
