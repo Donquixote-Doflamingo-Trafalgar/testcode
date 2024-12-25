065312971100948d8a6dceea95c2ceebb86b4db7
Trafalgar <omayosennin@gmail.com>
2024-12-25 23:35:15 +0530

    Add files via upload

---
+++ FITB game.py
@@ -0,0 +1,11 @@
+#story game
+
+adjective1 = input("Fill in the blanks with a suitable adjective in line 1 :     ")
+adverb1 = input("Fill in the blanks with a suitable adverb in line 2 :     ")
+noun1 = input("Fill in the blanks with a suitable noun in line 3 :     ")
+prepo1 = input("Fill in the blanks with a suitable preposition in line 4 :     ")
+prepo2 = input("Fill in the blanks with another preposition :     ")
+print(f"The door opened , and an {adjective1} man entered the room.")
+print(f"He looked very frightened, and he was breathing {adverb1}. ")
+print(f"Then I told him to sit down. His name was {noun1}.")
+print(f"He told me that a very strange occurence took place {prepo1} his house {prepo2} the 26th  of November this year.")
