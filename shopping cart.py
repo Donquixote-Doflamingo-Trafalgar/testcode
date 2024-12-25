d65d8b622916459e2543673b5c7eb6d54145770b
Donquixote-Doflamingo-Trafalgar <omayosennin@gmail.com>
2024-12-25 16:28:21 +0530

    Add files via upload

---
+++ shopping cart.py
@@ -0,0 +1,3 @@
+Price = float(input("What was the price?    "))
+Quantity = int(input("How many did you buy?   "))
+print(f"Your bill is {Quantity*Price*1.85} Japanese Yen.")
---
+++ v of a cuboid calculator.py
@@ -0,0 +1,5 @@
+#Calculator for the volume of a cuboid
+l = float(input("What's the length of the cuboid?    "))
+w = float(input("What's the width?    "))
+h = float(input("What's the height?   "))
+print(f"The volume of your cuboid is {l*w*h}cm (cubed)")
