668a2580e2e79fd2e986654ea683a31b8b7c463c
Trafalgar <omayosennin@gmail.com>
2024-12-27 17:37:02 +0530

    Add files via upload

---
+++ Try.py
@@ -0,0 +1,6 @@
+import math
+Player = "M. Salah"
+Goals = 21
+Assists = 15
+Matches = 28
+Ratio = print(f"The ratio of goals and assists (club and nation combined) per match of {Player} for the 2024-25 season is {round((Goals+Assists)/Matches, 3)}.")
