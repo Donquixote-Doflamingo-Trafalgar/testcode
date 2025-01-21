368803d59f2f97c59680c3da944db08938ca3fbe
Trafalgar <omayosennin@gmail.com>
2025-01-22 00:09:40 +0530

    Add files via upload

---
+++ Format specifiers.py
@@ -0,0 +1,10 @@
+Salah_ratio = 1.212
+Belloli_ratio = 1.03
+print(f"{Salah_ratio:.2f}")
+print(f"{round(Salah_ratio, 2)}")
+print(f"{Belloli_ratio:<99}")
+print(f"{Belloli_ratio:>99}")
+print(f"{Salah_ratio:0999}")
+print(f"{Belloli_ratio:^99}")
+print(f"{Salah_ratio:+}")
+print(f"{Salah_ratio:=}")
