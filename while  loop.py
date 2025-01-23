b59ca87b014bbf6aab171260dc938136f0a4bba2
Trafalgar <omayosennin@gmail.com>
2025-01-23 12:31:17 +0530

    Add files via upload

---
+++ while  loop.py
@@ -0,0 +1,28 @@
+#age = int(input("Enter your age:   "))
+#if age == 15:
+    #print("You are just too late to play in the clusters.")
+#elif age <= 14:
+    #print("You are eligible to play in the clusters.")
+#elif age > 15:
+    #print("You are overage.")
+#if age == 15:
+    #print("You are just too late to play in the clusters.")
+#while age == 15:
+    #print("afgisd")
+#if count < 5:
+    #print("Count is:", count)
+    #count += 1
+#count = 0
+#while count < 5:
+    #count += 1
+Goals_M = 24
+Goals_R = 25
+Assists_M = 17
+Assists_R = 11
+while Goals_R>Goals_M and Assists_R > Assists_M:
+    print("Raphinha D'Or!.")
+    New_G_M = int(input("How many more goals did Salah get? :    "))
+    New_A_M = int(input("How many more assists did Salah make? :         "))
+    Goals_M += New_G_M
+    Assists_M += New_A_M
+print("Salah D'or!")
